52b3d0946463cfe6d1fb948372a055ce26be01b7
JuTeLabs <julietech3@gmail.com>
2024-06-22 15:34:33 +0100

    Add initial backend code for portfolio

--- .history/script_20240622153226.js
+++ .history/script_20240622153226.js
@@ -72,30 +72,4 @@ $(document).ready(function() {
   });
 });
 
-// for the form
-
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-  event.preventDefault();
-
-  const formData = {
-    name: document.getElementById('name').value,
-    email: document.getElementById('email').value,
-    subject: document.getElementById('subject').value,
-    message: document.getElementById('message').value,
-  };
-
-  fetch('http://localhost:3000/send-message', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(formData),
-  })
-  .then(response => response.text())
-  .then(data => {
-    alert(data);
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-  });
-});
+for 
